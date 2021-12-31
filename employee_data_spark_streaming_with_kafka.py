b004b407ccd55c3400f632685f889fabe738794e
Manikandan Ganesan <95976596+mani2296@users.noreply.github.com>
2022-01-01 04:20:45 +0530

    Update and rename spark_streaming_with_kafka to employee_data_spark_streaming_with_kafka.py

--- employee_data_spark_streaming_with_kafka.py
+++ employee_data_spark_streaming_with_kafka.py
@@ -1,7 +1,18 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
+""" dataset:
+{"agent_id":1,"name":"Cliff","sal":100000,"desig":["SWE","Analyst","SAnalyst","PL"],"address":{"dno":4,"street":"Castlepoint blvd","area":"Piscatway","city":"NJ","pin":10043},"other_info":{"Marital Status":"M","Education":"MS","Language":"Eng"}}
+{"agent_id":2,"name":"Joe","sal":120000,"desig":["SWE","Analyst","SAnalyst","PM"],"address":{"dno":8,"street":"Camaron 3rd street","area":"Queens","city":"NY","pin":20088},"other_info":{"Marital Status":"M","Education":"BS","Language":"Eng"}}
+{"agent_id":3,"name":"Aravind","sal":130000,"desig":["SWE","Analyst","SAnalyst","PM"],"address":{"dno":3,"street":"Doveton street","area":"Purasai","city":"CHN","pin":600022},"other_info":{"Marital Status":"U","Education":"BE","Dept":"IT"}}
+{"agent_id":4,"name":"Gokul","sal":80000,"desig":["SWE","Analyst"],"address":{"dno":31,"street":"Mint Street","area":"Broadway","city":"CHN","pin":600001},"other_info":{"Marital Status":"U","Education":"BSc","Bond":"Yes"}}
 
+"""
+"""Description
+In this employee details dataset we get the data from kafka topic. Data from kafka will be in key value pairs and also in binary format so we convert that to appropriate format 
+using from_json function(for from_json function u need to pass schema as argument) after that we applied some basic cleaning of data and output the result again back to the 
+kafka. Before sending data to kafka u need to convert the result set into key value pairs as kafka accepts only key value pairs.
+""""
 if __name__ == "__main__":
     spark=SparkSession.builder.master("local[*]")\
         .config("spark.streaming.stopGracefullyOnShutdown","true")\
@@ -33,7 +44,7 @@ if __name__ == "__main__":
     
     df_out = clean_df.select(expr("agent_id as key"),to_json(struct("*")).alias("value"))
     
-    kafka_write=df_out.writeStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").outputMode("update").triger("processingTime=330 seconds") \
+    kafka_write=df_out.writeStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").outputMode("update").triger("processingTime=30 seconds") \
                 .option("topic","invoiceout").option("checkpointLocation","/home/hduser/checkpoint_kafka").start()
             
     kafka_write.awaitTermination()
